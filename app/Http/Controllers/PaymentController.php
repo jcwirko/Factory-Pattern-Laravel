aa4706e5b47f507c7b1a257dc335db20070ece9a
juanblariza <juanblariza@gmail.com>
2021-07-14 16:00:33 -0300

    Refactoriza el controlador

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Factories\PaymentFactory;
 use App\Models\Payment;
 use App\Repositories\PaymentRepository;
 use App\Repositories\ProductRepository;
@@ -11,76 +12,20 @@ use Illuminate\Support\Collection;
 
 class PaymentController extends Controller
 {
-    private $tax;
-    private $currency;
     private $paymentRepository;
-    private $paymentService;
-    private $productRepository;
+    private $paymentFactory;
 
-    public function __construct(
-        PaymentRepository $paymentRepository,
-        PaymentService $paymentService,
-        ProductRepository $productRepository
-    ) {
+    public function __construct(PaymentRepository $paymentRepository, PaymentFactory $paymentFactory)
+    {
         $this->paymentRepository = $paymentRepository;
-        $this->paymentService = $paymentService;
-        $this->productRepository = $productRepository;
-        $this->currency = 'usd';
-        $this->tax = 5;
+        $this->paymentFactory = $paymentFactory;
     }
 
     public function add()
     {
-        $products = $this->productRepository->getByIds(request()->input('products_ids'));
-        $subtotal = $products->sum('price');
-        $amount = $this->addTaxToSubTotal($subtotal);
-        $paymentMetadata = $this->generatePaymentMetadata($products, $subtotal);
-
-        $paymentIntent = $this->paymentService->createPaymentIntent(
-            $amount,
-            $this->currency,
-            $paymentMetadata
-        );
-
-        $paymentInstance = $this->getPaymentInstance([
-            'status' => PaymentStatusValue::PENDING,
-            'amount' => $amount,
-            'products' => request()->input('products'),
-            'payment_platform_id' => $paymentIntent['id'],
-            'detail' => json_encode($paymentIntent),
-            'user_id' => request()->input('user_id')
-        ]);
-
+        $paymentInstance = $this->paymentFactory->createInstance(request()->input('products_ids'));
         $payment = $this->paymentRepository->save($paymentInstance);
 
         return response()->json($payment);
     }
-
-    private function generatePaymentMetadata(Collection $products, float $subtotal): Collection
-    {
-        $metadata[] = $products->mapWithKeys(function($product) {
-            return [
-                str_replace(' ', '', ucwords($product['name'])) => $product['price']
-            ];
-        })->toArray();
-
-        $metadata[] = [
-            'subtotal' => $subtotal,
-            'tax' => "$this->tax%",
-            'amount' => $this->addTaxToSubTotal($subtotal)
-        ];
-
-        return collect(array_merge(...$metadata));
-    }
-
-    private function getPaymentInstance(array $attributes): Payment
-    {
-        return new Payment($attributes);
-    }
-
-    private function addTaxToSubTotal(int $price)
-    {
-        return $price * (1 + $this->tax / 100);
-    }
-
 }
